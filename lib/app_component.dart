157b470c339b8d13ae0b26caf2157a6fba2377d9
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-06-07 01:35:48 +0300

    Init 2

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,8 @@
 import 'package:angular2/core.dart';
 @Component(
-    selector: 'my-app',
-    template: '<h1>Hello world</h1>')
-class AppComponent {}
+    selector: 'my-directive',
+    template: '<h1>Hello, I am {{name}}. I am {{age}}</h1>')
+class AppComponent {
+  String name = 'Vlad';
+  int age = 22;
+}
