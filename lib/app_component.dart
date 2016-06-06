06138be9380067236c16086beb66741ac744d008
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-06-07 01:30:13 +0300

    Initial

---
+++ lib/app_component.dart
@@ -0,0 +1,5 @@
+import 'package:angular2/core.dart';
+@Component(
+    selector: 'my-app',
+    template: '<h1>Hello world</h1>')
+class AppComponent {}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/platform/browser.dart';
+import 'package:ng2_hero_app/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
