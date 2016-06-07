cfce0a1e8b1f94b7c5d37120850a2e49395fdb3b
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-06-08 00:33:53 +0300

    Try list of heroes

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,13 @@
+// #docregion pt1
 import 'package:angular2/platform/browser.dart';
+
 import 'package:ng2_hero_app/app_component.dart';
 
-void main() {
-  bootstrap(AppComponent);
+main() {
+  bootstrap(AppComponent);/*
+  List<Hero> heroes = AppComponent.heroes;
+  for (int i = 0; i < heroes.length; i++) {
+    print(heroes[i].name);
+  }*/
 }
+// #enddocregion pt1
