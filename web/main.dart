7aa6ed3f2a13676b16982510e405107fdea56a87
Sharikov Vladislav <vlad.spb.sh@gmail.com>
2016-06-09 00:46:29 +0300

    Try logging package

--- web/main.dart
+++ web/main.dart
@@ -3,8 +3,23 @@ import 'package:angular2/platform/browser.dart';
 
 import 'package:ng2_hero_app/app_component.dart';
 
+import 'package:logging/logging.dart';
+
+
 main() {
-  bootstrap(AppComponent);/*
+  bootstrap(AppComponent);
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
+  final Logger log = new Logger('MyClassName');
+
+  log.info('info');
+  log.warning('warning');
+
+  /*
   List<Hero> heroes = AppComponent.heroes;
   for (int i = 0; i < heroes.length; i++) {
     print(heroes[i].name);
